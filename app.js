ba13fdff78c1369419db9c99613d06562603a22c
Aymane Kherrati <68582060+Kerrmane@users.noreply.github.com>
2022-10-06 14:33:01 +0200

    fetsh js

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-
-async function main(){
- const cat = await  fetch('https://catfact.ninja/breeds?limit=12')
-.then(resultat => resultat.json())
-.then(json => json)
-console.log(cat);
-function getInfo(cat){
-    const breed = data.breed ;
-    const country = data.country ;
-    const origin = data.origin ;
-    const coat = data.coat ;
-    document.querySelector('#breed').textContent = breed;
-    document.getElementById("breed").innerHTML = "Hello World!";
-    console.log("breed cat is : " + breed);
-}
-
-
+function main(){
+    fetch('https://catfact.ninja/breeds?limit=1')
+    .then(resultat => resultat.json())
+    .then(resultat => console.log(JSON.stringify(resultat)))
+    .catch(error => alert("erreur  " + error))
 
 }
-
-
-
 main();
-
-
-
