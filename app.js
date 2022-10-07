2bd2f39db69f9d4a0095151ecafcda7fdb1f2d6e
Aymane Kherrati <68582060+Kerrmane@users.noreply.github.com>
2022-10-07 14:42:13 +0200

    js function

--- app.js
+++ app.js
@@ -8,10 +8,64 @@ async function getBreeds() {
     })
    
 }
-
+ function lengthArray(breeds){
+    return breeds.length
+    
+}
 async function onInit(){
     let breeds = await getBreeds();
-    let length = breeds.length
-    console.log(length)
+    console.log(breeds);
+
+    let longeur =lengthArray(breeds)
+    console.log(longeur)
+    
+    let countries = getCountries(breeds);
+    console.log(countries);
+
+    let breed = getBreed(breeds);
+    console.log(breed);
+
+    let pattern = getPattern(breeds);
+    console.log(pattern);
+
+    let coat = getCoat(breeds);
+    console.log(coat);
+
+    let origine = getOrigine(breeds);
+    console.log(origine);
 }
+function getCountries(breeds){
+    
+    let pays=[];
+    breeds.forEach(breed => pays.push(breed.country))
+    pays = pays.filter((item, index) => pays.indexOf(item) === index);
+    return pays ;
+}
+function getBreed (breeds){
+    let breedName=[];
+    breeds.forEach(breed => breedName.push(breed.breed))
+    return breed ;
+
+}
+function getOrigine (breeds){
+    let origine=[];
+    breeds.forEach(breed => origine.push(breed.origin))
+    return origine ;
+    
+}
+function getCoat (breeds){
+    let coat=[];
+    breeds.forEach(breed => coat.push(breed.coat))
+    return coat ;
+    
+}
+function getPattern (breeds){
+    let pattern=[];
+    breeds.forEach(breed => pattern.push(breed.pattern))
+    return pattern ;
+    
+}
+ 
 onInit();
+
+
