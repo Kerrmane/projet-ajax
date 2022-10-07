0a3832467392394398604ec1427bc55f39b1eceb
Aymane Kherrati <68582060+Kerrmane@users.noreply.github.com>
2022-10-07 10:39:25 +0200

    fetsh js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-function main(){
-    fetch('https://catfact.ninja/breeds?limit=1')
-    .then(resultat => resultat.json())
-    .then(resultat => console.log(JSON.stringify(resultat)))
-    .catch(error => alert("erreur  " + error))
-
+async function getBreeds() {
+    return new Promise((resolve) => {
+        fetch("https://catfact.ninja/breeds?limit=50")
+        .then(resultat => resultat.json())
+        .then(resultat => {
+            resolve(resultat.data);
+        })
+    })
+   
 }
-main();
+
+async function onInit(){
+    let breeds = await getBreeds();
+    var length = Object.keys(myObj).length;
+    console.log(length(breeds));
+}
+onInit();
